74a883d1c30c06c6f8c984aa00153e69994c60b6
Hannes Steffenhagen <hsteffe@us.ibm.com>
2015-06-26 13:46:10 -0400

    Fix wrong test description

--- test/FibonacciSpec.hs
+++ test/FibonacciSpec.hs
@@ -17,6 +17,6 @@ spec = do
     it "should start with 0" $ do
       head fibonaccis `shouldBe` 0
     it "should contain 3, 8, 34 in the first 10 elements" $ do
-      take 10 fibonaccis `shouldContainInAnyOrder` [8,5,34]
+      take 10 fibonaccis `shouldContainInAnyOrder` [8,8,34]
     it "should satisfy the definition fib(n) = fib(n-1) + fib(n-2)" $
       property $ \n -> n >= 2 ==> fibonaccis !! n == fibonaccis !! (n-1) + fibonaccis !! (n-2)
